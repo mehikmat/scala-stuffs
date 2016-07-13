57f55b85a5cb80c1367a0e105d0b2d2063f966b5
Hikmat <hdhamee@deerwalk.com>
2016-07-13 15:38:00 +0545

    adding scala demo

--- scala-basics/src/main/scala/demo/HelloWorld.scala
+++ scala-basics/src/main/scala/demo/HelloWorld.scala
@@ -1,10 +1,14 @@
 package demo
+
+import java.io.{FileNotFoundException, File, PrintWriter, FileReader, IOException}
+
 import demo.Run._
+
+import scala.io.Source
 
 object HelloWorld {
 
   def main(args: Array[String]) = {
-
     var myVar :Int = 10 // variable/mutable declaration
     val myVal :String = "Hello Scala with datatype declaration." // constant/immutable declaration
     println(myVar); println(myVal)
@@ -68,11 +72,45 @@ object HelloWorld {
   println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
 
   //Exception test
+  //Checked Exception: checked at compile time by compiler and throws error if not handled or declared at method signature
+  //Unchecked Exception: Not checked by compiler at compile time. Eg. Runtime Exceptions
+
+  try {
+    val f = new FileReader("input.txt")
+  } catch {
+    case ex: FileNotFoundException => {
+      println("Missing file exception")
+    }
+
+    case ex: IOException => {
+      println("IO Exception")
+    }
+  } finally {
+    println("Exiting finally...")
+  }
+  println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
 
 
   // Extractor test
 
 
   // File I/O test
+  // using java classes
+  val writer = new PrintWriter(new File("Test.iml" ))
+  writer.write("Hello Scala")
+  writer.close()
+  println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+
+  //using scala
+  print("Please enter your input : " )
+  val line = Console.readLine()
+  println("Thanks, you just typed: " + line)
+  println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+
+
+  println("Following is the content read:" )
+  Source.fromFile("Test.iml" ).foreach {print};println
+  println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+
 
 }
